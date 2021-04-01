3f9b35b27e400c9e421df71faad671640af04667
Kevin Leung <kevinresol@gmail.com>
2021-04-01 23:12:38 +0800

    Some updates

--- src/google/cloud/Firestore.hx
+++ src/google/cloud/Firestore.hx
@@ -9,8 +9,9 @@ extern class Firestore {
 	function collection<T>(name:String):CollectionReference<T>;
 }
 extern class CollectionReference<T> extends Query<T> {
-	var id(default, never):String;
+	final id:String;
 	function doc(?name:String):DocumentReference<T>;
+	function add(data:T):Promise<DocumentReference<T>>;
 }
 extern class Query<T> {
 	function where(path:String, op:String, value:Any):Query<T>;
@@ -19,15 +20,16 @@ extern class Query<T> {
 	function get():Promise<QuerySnapshot<T>>;
 }
 extern class DocumentReference<T> {
-	var id(default, never):String;
+	final id:String;
 	function get():Promise<DocumentSnapshot<T>>;
-	function update(v:Dynamic):Promise<WriteResult>;
+	function update(v:Partial<T>):Promise<WriteResult>;
 	function set(v:T):Promise<WriteResult>;
 	function delete():Promise<WriteResult>;
+	function collection<U>(name:String):CollectionReference<U>;
 }
 extern class DocumentSnapshot<T> {
-	var id(default, never):String;
-	var exists(default, never):Bool;
+	final id:String;
+	final exists:Bool;
 	function data():T;
 }
 extern class QueryDocumentSnapshot<T> extends DocumentSnapshot<T> {}
@@ -35,10 +37,14 @@ extern class QueryDocumentSnapshot<T> extends DocumentSnapshot<T> {}
 extern class QuerySnapshot<T> {
 	final docs:Array<QueryDocumentSnapshot<T>>;
 }
-extern class WriteResult {}
+extern class WriteResult {
+	final writeTime:Timestamp;
+}
 
 @:jsRequire('@google-cloud/firestore', 'Timestamp')
 extern class Timestamp {
 	static function fromDate(date:Date):Timestamp;
 	function toDate():Date;
 }
+
+typedef Partial<T> = Dynamic;
